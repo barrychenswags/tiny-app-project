d2c9ce77fa8b0e2fc91d58a41818342222940539
Barry <barry2014158@gmail.com>
2019-06-05 19:37:54 +0000

    edit files

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+app.set("view engine", "ejs");
+
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -11,9 +13,33 @@ var urlDatabase = {
   res.send("Hello!");
 });*/
 
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
+
+/*
+app.get("/new", (req, res) => {
+  res.render("new_urls")
+});
+
+/*
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
